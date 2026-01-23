6ca2f7483ac763e50fdebce148af4e0d42729e9c
nkurunziza-saddy <saadnkurunziza@gmail.com>
2026-01-23 09:27:15 +0200

    Clean reset: overwrite repo with fresh project

---
+++ apps/web/src/components/navigation.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div className='flex '>
+        <li>Home</li>
+        <li>About</li>
+    </div>
+  )
+}
+
+export default Navigation
